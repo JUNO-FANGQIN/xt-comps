25946eff55b84322b7dccd7bbbbe7d8b342940a1
juno.fang <1479351447@qq.com>
2021-09-27 17:27:32 +0800

    icon storybook

--- src/components/Icon/index.stories.tsx
+++ src/components/Icon/index.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Icon from './index';
+import Icon, { EMIconType } from './index';
 
 export default {
   title: 'Example/Components/Icon',
@@ -10,7 +10,18 @@ export default {
 
 const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
-  id: 'id'
-};
+export const Default = () => {
+  const types = Object.values(EMIconType)
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row'}}>
+      {types.map((type: EMIconType)  => {
+        return (
+          <div style={{ margin: 20, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Icon id="key" type={type} />
+            <div>icon-{type}</div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
